60e74e10893153062ba3ab95532c8b844e53cd91
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-15 16:48:45 +0100

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import theme from "./chakra/theme";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+const LocationProvider = ({ children }) => {
+  return <AnimatePresence>{children}</AnimatePresence>;
+};
+
+const RoutesWithAnimation = () => {
+  const location = useLocation();
+  return (
+    <Routes location={location} key={location.key}>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:id" element={<Products />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/About/" element={<About />} />
+      <Route path="/Contact/" element={<Contact />} />
+    </Routes>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+        <LocationProvider>
+          <RoutesWithAnimation />
+        </LocationProvider>
+        <Footer />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
