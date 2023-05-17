e60db69262ac31b6b3439ec0cf79ae790d72b216
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-17 10:11:49 +0100

    lists of products in product section

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Card, { api } from "../Card/Card";
+import PostLoader from "../PostLoader/PostLoader";
+import { Flex } from "@chakra-ui/react";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const [data, setPhotosResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default List
+  const fetchImageFromApi = async () => {
+    try {
+      setLoading(true);
+      const response = await api.search.getPhotos({
+        query: "products",
+        perPage: 6,
+        orientation: "landscape",
+      });
+      setPhotosResponse(response);
+    } catch (error) {
+      console.log("fetchImageFromApi error: ", error.message);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchImageFromApi();
+  }, []);
+
+  if (data === null) {
+    return <PostLoader />;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  }else{
+
+    return (
+    <Flex wrap="wrap" align="center" gap={{base:"48px", md:"20px", lg:"48px"}}>
+      {
+        data.response.results.map((photo, idx) => (
+          <Card key={idx} photo={photo} />
+        ))
+      }
+    </Flex>
+    );
+  }
+};
+
+export default List;
