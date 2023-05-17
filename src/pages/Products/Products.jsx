9b45837ebbba82ccbc2e4e9119d259e27f3b0572
BlackBond06 <eleazuosita007@yahoo.com>
2023-05-17 10:12:13 +0100

    product page completed

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -13,7 +13,9 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
+import List from "../../components/List/List";
+import { useParams } from "react-router-dom";
 
 const routerVariants = {
   initial: {
@@ -25,60 +27,80 @@ const routerVariants = {
 };
 
 const Products = () => {
+  const categoryId = Number(useParams().id);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+
   return (
     <motion.div variants={routerVariants} initial="initial" animate="final">
-      <Flex>
-        <Flex direction="column">
+      <Flex  direction={{base:"column", md:"unset"}} padding={{base:"unset", md:"unset", lg:"30px 50px"}}>
+        <Flex direction="column" flex={1} position="sticky"
+        height={{base:"auto", md:"auto", lg:"100%"}}
+        top="50px"
+        padding={{base:"30px 50px", md:"30px 10px", lg:"unset"}}
+        zIndex={3}
+        backgroundColor={{base:"gray.100", md:"gray.100", lg:"unset"}}
+        >
           <Box>
-            <Heading fontSize="14px">Product Categories</Heading>
-            <Flex>
+            <Heading fontSize="14px"  marginBottom="20px">Product Categories</Heading>
+            <Flex marginBottom="10px">
               <Checkbox value={1} />
-              <Tag>
+              <Tag marginLeft="10px" background="inherit">
                 <TagLabel>Shoes</TagLabel>
               </Tag>
             </Flex>
-            <Flex>
+            <Flex marginBottom="10px">
               <Checkbox value={1} />
-              <Tag>
-                <TagLabel>Gowns</TagLabel>
+              <Tag marginLeft="10px" background="inherit">
+                <TagLabel >Gowns</TagLabel>
               </Tag>
             </Flex>
-            <Flex>
+            <Flex align="center">
               <Checkbox value={1} />
-              <Tag>
+              <Tag marginLeft="10px" background="inherit">
                 <TagLabel>Suits</TagLabel>
               </Tag>
             </Flex>
           </Box>
-          <Box>
-            <Heading fontSize="14px">Filter by price</Heading>
+          <Box marginBottom="30px">
+            <Heading fontSize="14px" marginBottom="20px" marginTop="20px">Filter by price</Heading >
             <Flex gap={2} width="200px">
               <Text>0</Text>
-              <RangeSlider aria-label={["min", "max"]} defaultValue={[0, 1000]}>
+              <RangeSlider aria-label={["min", "max"]} defaultValue={[0, 1000]} onChange={(e)=> setMaxPrice(e[1])}>
                 <RangeSliderTrack>
                   <RangeSliderFilledTrack />
                 </RangeSliderTrack>
                 <RangeSliderThumb index={0} />
                 <RangeSliderThumb index={1} />
               </RangeSlider>
-              <Text>1000</Text>
+              <Text>{maxPrice}</Text>
             </Flex>
           </Box>
           <Box>
-            <Heading fontSize="14px">Sort by</Heading>
-            <Box>
-              <input type="radio" id="asc" value="asc" name="price"/>
+            <Heading fontSize="14px" marginBottom="20px">Sort by</Heading>
+            <Flex gap="10px">
+              <input type="radio" id="asc" value="asc" name="price" onChange={(e)=> setSort("asc")}/>
               <label htmlFor="asc" >Price (Lowest first)</label>
-            </Box>
-            <Box>
-              <input type="radio" id="desc" value="asc" name="price"/>
+            </Flex>
+            <Flex gap="10px">
+              <input type="radio" id="desc" value="asc" name="price" onChange={(e)=> setSort("desc")}/>
               <label htmlFor="desc" >Price (Highest first)</label>
-            </Box>
+            </Flex>
           </Box>
         </Flex>
-        <Flex direction="column">
-          <Image src="https://images.unsplash.com/photo-1627384113972-f4c0392fe5aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        <Flex  direction="column" flex={3}>
+          <Box height="300px" marginBottom="50px">
+          <Image src="https://images.unsplash.com/photo-1627384113972-f4c0392fe5aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+          height="100%"
+          width="100%"
+          objectFit="cover"
+          />
+          </Box>
+          <Box padding={{base:"30px 50px", md:"20px 12px", lg:"unset"}}>
 
+          <List categoryId={categoryId} maxPrice={maxPrice} sort={sort}/>
+          </Box>
+          
         </Flex>
       </Flex>
     </motion.div>
